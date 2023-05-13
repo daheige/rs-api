ae70fecc1b8a4e9adea5b074c9d2512f0cb3359a
daheige <zhuwei313@hotmail.com>
2023-05-13 22:54:54 +0800

    feat: cookie set/get

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -1,5 +1,6 @@
 use crate::entity::user;
 use crate::services::user as userService;
+use axum::http::{header, HeaderMap};
 use axum::{
     http::StatusCode,
     response::{Html, IntoResponse},
@@ -76,4 +77,67 @@ pub async fn accept_form(Form(input): Form<user::UserForm>) -> impl IntoResponse
             data: Some(input),
         }),
     )
+}
+
+pub async fn set_user_cookie() -> impl IntoResponse {
+    let user = user::User {
+        id: 1,
+        username: "daheige".to_string(),
+    };
+
+    let mut headers = HeaderMap::new();
+    let cookie = format!("{}={}", "user_name", user.username);
+    headers.insert(header::SET_COOKIE, cookie.as_str().parse().unwrap());
+
+    // redirect to / with cookie
+    // headers.insert(header::LOCATION, "/".parse().unwrap());
+    // (StatusCode::FOUND, headers, ())
+
+    // returns json and set cookie
+    (
+        StatusCode::OK,
+        headers,
+        Json(super::Reply {
+            code: 0,
+            message: "login success".to_string(),
+            data: Some(super::EmptyObject {}),
+        }),
+    )
+}
+
+pub async fn get_user_cookie(headers: HeaderMap) -> impl IntoResponse {
+    let cookies = headers
+        .get(header::COOKIE)
+        .and_then(|v| v.to_str().ok())
+        .map(|v| v.to_string())
+        .unwrap_or("".to_string()); // get all cookie
+    if cookies.is_empty() {
+        return Err("cookies is empty"); // no Cookie
+    }
+
+    let mut username: Option<String> = None;
+    let cookies: Vec<&str> = cookies.split(';').collect(); // split with ;
+    for cookie in cookies {
+        let cookie_pair: Vec<&str> = cookie.split('=').collect(); // splice with =
+        let cookie_name = cookie_pair[0].trim();
+        let cookie_value = cookie_pair[1].trim();
+        // cookie no empty
+        if cookie_name == "user_name" && !cookie_value.is_empty() {
+            username = Some(String::from(cookie_value));
+            break;
+        }
+    }
+
+    if username.is_none() {
+        return Err("username cookie is empty");
+    }
+
+    Ok((
+        StatusCode::OK,
+        Json(super::Reply {
+            code: 0,
+            message: "ok".to_string(),
+            data: Some(username),
+        }),
+    ))
 }
--- src/routes/router.rs
+++ src/routes/router.rs
@@ -13,7 +13,10 @@ pub fn api_router() -> Router {
         .route("/empty-array", get(handlers::index::empty_array))
         .route("/users", post(handlers::index::create_user))
         .route("/html", get(handlers::index::html_foo))
-        .route("/form", post(handlers::index::accept_form));
+        .route("/form", post(handlers::index::accept_form))
+        .route("/set-user-cookie", get(handlers::index::set_user_cookie))
+        // .route("/set-user-cookie", post(handlers::index::set_user_cookie))
+        .route("/get-user-cookie", get(handlers::index::get_user_cookie));
 
     // handler not found
     app = app.fallback(not_found_handler);
