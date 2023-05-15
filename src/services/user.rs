5a47711ef887333093a27f346f7daf9f332c1e27
heige <zhuwei313@hotmail.com>
2023-05-15 15:27:04 +0800

    feat: Update user.rs

--- src/services/user.rs
+++ src/services/user.rs
@@ -6,7 +6,7 @@ pub fn set_user(user: &entity::user::User) -> RedisResult<String> {
     let mut conn = config::REDIS_POOL.get().unwrap();
 
     // 设置单个pool timeout
-    // let mut conn = pool.get_timeout(Duration::from_secs(2)).unwrap();
+    // let mut conn = config::REDIS_POOL.get_timeout(Duration::from_secs(2)).unwrap();
     let res: RedisResult<String> = conn.set(
         "my_user:".to_string() + &user.id.to_string(),
         user.username.as_str(),
